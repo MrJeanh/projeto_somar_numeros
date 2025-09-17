71d66213abecc2ac92e1006677608ffff59c4dfc
Jean <mrjean.hw@gmail.com>
2025-09-16 21:02:09 -0300

    Modificando

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import funcoes as f
+
+"""
+Este programa solicita números do usuário,
+soma os dígitos e exibe o resultado.
+Exemplo: 54 -> 5 + 4 = 9
+"""
+
 print('Esse Programa foi desenvolvido como forma de aprendizado!')
 print('Nele o usuario deve fornecer alguns números e o programa irá somar eles')
 print('Exemplo: 54, será feita soma de 5 + 4, resultando 9.')
+
 def main():
     soma = input('Digite alguns números: ')
     if soma.isdigit() and f.numeros_digitos(soma) == True:
